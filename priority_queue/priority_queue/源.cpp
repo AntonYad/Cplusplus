54532cedd283f87eb629616365caf889af34258c
Anton <651299847@qq.com>
2021-04-16 07:45:37 +0800

    priority_queue

---
+++ priority_queue/priority_queue/源.cpp
@@ -0,0 +1,94 @@
+#pragma once
+#include<vector>
+namespace GX
+{
+	template<class T,class Container = vector<T>,class Compare= less<T>>// Compare =greater<T>
+	class priority_queue
+	{
+	public:
+		void Adjustdown(int root)
+		{
+			Compare com;
+			int parents = root;
+			child = parents * 2 + 1;
+			if (child+1<_com.size()&&_con[child] > _con[child + 1])
+			{
+				child++;
+			}
+			while (child<_con.size())
+			{
+				if (_con[child] < _con[parents])//if(_con[child],_con[parents])
+				{
+					swap(_con[child], _con[parents]);
+					parents = child;
+					child = parents * 2 + 1;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		void Adjustup(int child)
+		{
+			Compare com;
+			int parents = (child - 1) / 2;
+			while (child>0)
+			{
+				if (_con[child] >_con[ parents])//if(_con[parents],_con[child])
+				{
+					swap(_con[parents],_con[ child]);
+					child = parents;
+					parents = (child - 1) / 2;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		void push(const T& x)
+		{
+			_con.push.back(x);
+			Adjustup(_con.size() - 1);
+		}
+		void pop()
+		{
+			swap(_con[0], _con[_con.size() - 1]);
+			_con.pop_back();
+			Adjustdown(0);
+		}
+		T& top()
+		{
+			return _con.front();
+		}
+
+		size_t size()
+		{
+			return _con.size();
+		}
+		bool empty()
+		{
+			return _con.empty();
+		}
+	private:
+		Container _con;
+	};
+}
+//仿函数（优先级队列 priority）
+template<class T>
+struct less
+{
+	bool opreator(const T& x1,const T& x2 )
+	{
+		return x1 < x2;
+	}
+};
+template<class T>
+struct greater
+{
+	bool opreator(const T& x1, const T& x2)
+	{
+		return x1 > x2;
+	}
+};
