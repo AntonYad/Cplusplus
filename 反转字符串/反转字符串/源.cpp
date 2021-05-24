5d5ca1a7001122a8f411db5c01185dd12fb307c6
AntonYad <651299847@qq.com>
2021-05-24 16:14:54 +0800

    Makefile

--- 反转字符串/反转字符串/源.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-	void reverseString(vector<char>& s) {
-		int length = sizeof(s);
-		int e = 0;
-		reverse(s.begin(), s.end());
-	}
-};
--- 最小公倍数/最小公倍数/源.cpp
+++
@@ -1,2 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>int main(){    int A, B;    scanf("%d%d", &A, &B);    int i = 1;    while (((A * i) % B) != 0)    {i++;    }    printf("%d\n", A*i);    return 0;}
